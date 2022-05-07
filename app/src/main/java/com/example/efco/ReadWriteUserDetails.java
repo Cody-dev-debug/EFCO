572de90ef8f05e085136d9d183bd95e60a4ee52d
Aditya <bansaladitya150@gmail.com>
2022-05-07 15:42:20 +0530

    Third Commit

---
+++ app/src/main/java/com/example/efco/ReadWriteUserDetails.java
@@ -0,0 +1,13 @@
+package com.example.efco;
+
+public class ReadWriteUserDetails {
+
+    public String fullname,phone,address;
+
+    public ReadWriteUserDetails(String textFullname, String textPhone, String textAddress)
+    {
+        this.fullname=textFullname;
+        this.phone=textPhone;
+        this.address=textAddress;
+    }
+}
